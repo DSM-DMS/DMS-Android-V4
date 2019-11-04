d96877bbfee976e6a3e61e51295c754ffdbee4ee
jinusong <sugwer7464@gmail.com>
2019-11-04 16:58:38 +0900

    [Add] 잔류 신청, 신청 메인 ViewModel
    Presentation 로직을 수행할 ViewModel과 그의 Factory

---
+++ presentation/src/main/java/com/dsm/dms/presentation/viewmodel/main/apply/main/ApplyMainViewModel.kt
@@ -0,0 +1,7 @@
+package com.dsm.dms.presentation.viewmodel.main.apply.main
+
+import com.dsm.dms.presentation.base.BaseViewModel
+
+class ApplyMainViewModel: BaseViewModel() {
+
+}
---
+++ presentation/src/main/java/com/dsm/dms/presentation/viewmodel/main/apply/staying/ApplyStayingViewModel.kt
@@ -0,0 +1,7 @@
+package com.dsm.dms.presentation.viewmodel.main.apply.staying
+
+import com.dsm.dms.presentation.base.BaseViewModel
+
+class ApplyStayingViewModel: BaseViewModel() {
+
+}
