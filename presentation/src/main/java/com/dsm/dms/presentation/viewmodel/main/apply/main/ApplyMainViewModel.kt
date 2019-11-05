fdc223c6d016aef1a96027ddb5c3a002c1f1d218
jinusong <sugwer7464@gmail.com>
2019-11-05 09:30:52 +0900

    [Update] ApplyMain의 ui와 Event 정의
    xml로 layout을 만들고 프래그먼트와 ViewModel 작성

--- presentation/src/main/java/com/dsm/dms/presentation/viewmodel/main/apply/main/ApplyMainViewModel.kt
+++ presentation/src/main/java/com/dsm/dms/presentation/viewmodel/main/apply/main/ApplyMainViewModel.kt
@@ -1,7 +1,18 @@
 package com.dsm.dms.presentation.viewmodel.main.apply.main
 
 import com.dsm.dms.presentation.base.BaseViewModel
+import com.dsm.dms.presentation.base.SingleLiveEvent
 
 class ApplyMainViewModel: BaseViewModel() {
+    val goStayingEvent = SingleLiveEvent<Unit>()
+    val goStudyingEvent = SingleLiveEvent<Unit>()
+    val goGoingOutEvent = SingleLiveEvent<Unit>()
+    val goMusicEvent = SingleLiveEvent<Unit>()
+    val goRestEvent = SingleLiveEvent<Unit>()
 
+    fun stayingClick() = goStayingEvent.call()
+    fun studyingClick() = goStudyingEvent.call()
+    fun goingOutClick() = goGoingOutEvent.call()
+    fun musicClick() = goMusicEvent.call()
+    fun restClick() = goRestEvent.call()
 }
