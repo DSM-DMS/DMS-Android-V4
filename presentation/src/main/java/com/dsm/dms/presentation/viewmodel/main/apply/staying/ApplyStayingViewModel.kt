26eaa9e12c82530b2e87065d1fba72ebf4cc22cc
jinusong <sugwer7464@gmail.com>
2019-11-05 20:30:02 +0900

    [Update] 잔류 신청 기능 구현
    함수 바인딩 및 View Fragment에서의 이벤트 구현

--- presentation/src/main/java/com/dsm/dms/presentation/viewmodel/main/apply/staying/ApplyStayingViewModel.kt
+++ presentation/src/main/java/com/dsm/dms/presentation/viewmodel/main/apply/staying/ApplyStayingViewModel.kt
@@ -1,7 +1,30 @@
 package com.dsm.dms.presentation.viewmodel.main.apply.staying
 
+import androidx.lifecycle.MutableLiveData
 import com.dsm.dms.presentation.base.BaseViewModel
+import com.dsm.dms.presentation.base.SingleLiveEvent
 
 class ApplyStayingViewModel: BaseViewModel() {
+    val nowStayingState = MutableLiveData<String>()
 
+    val cardChangeColorEvent = SingleLiveEvent<String>()
+    val backToMainEvent = SingleLiveEvent<Unit>()
+
+    fun fridayGoClick() {
+        cardChangeColorEvent.value = "금요귀가"
+    }
+
+    fun saturdayGoClick() {
+        cardChangeColorEvent.value = "토요귀가"
+    }
+
+    fun saturdayBackClick() {
+        cardChangeColorEvent.value = "토요귀사"
+    }
+
+    fun stayingClick() {
+        cardChangeColorEvent.value = "잔류"
+    }
+
+    fun backClick() = backToMainEvent.call()
 }
