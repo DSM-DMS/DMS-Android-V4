caeb03baa3ad8132d09f414d839b11a17ed10483
jinusong <sugwer7464@gmail.com>
2020-05-30 16:40:01 +0900

    [Add] 휴식 기능 UI 로직 추가

--- presentation/src/main/java/com/dsm/dms/presentation/ui/fragment/apply/main/ApplyMainFragment.kt
+++ presentation/src/main/java/com/dsm/dms/presentation/ui/fragment/apply/main/ApplyMainFragment.kt
@@ -41,7 +41,7 @@ class ApplyMainFragment: DataBindingFragment<FragmentApplyMainBinding>() {
 
         })
         viewModel.goRestEvent.observe(this, Observer {
-
+            findNavController().navigate(R.id.action_applyMainFragment_to_applyWeekendRestFragment)
         })
     }
 }
