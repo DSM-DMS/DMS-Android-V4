782872225186f82184e48f9eb34bbc8eee77cc86
jinusong <sugwer7464@gmail.com>
2020-06-16 23:25:30 +0900

    Update DataBinding Base Code

--- presentation/src/main/java/com/dsm/dms/presentation/base/DataBindingInjectFragment.kt
+++ presentation/src/main/java/com/dsm/dms/presentation/base/DataBindingInjectFragment.kt
@@ -1,35 +1,33 @@
 package com.dsm.dms.presentation.base
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.AndroidSupportInjection
-import dagger.android.support.DaggerDialogFragment
 import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
-abstract class DataBindingDialogFragment<T : ViewDataBinding> : DialogFragment(), HasSupportFragmentInjector {
+abstract class DataBindingInjectFragment<T : ViewDataBinding> : DataBindingFragment<T>(), HasSupportFragmentInjector {
 
     @Inject
     lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
 
-    lateinit var rootView: View
-    lateinit var binding: T
-
-    abstract val layoutId: Int
+    private val lifecycleOwner = LifecycleOwner()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         AndroidSupportInjection.inject(this)
-        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
-        binding.lifecycleOwner = this
-        rootView = binding.root
+        super.onCreateView(inflater, container, savedInstanceState)
         return rootView
     }
 
