e9aa9b01fd6c76a32d56372bfcac4f760ec52f84
jinusong <sugwer7464@gmail.com>
2020-08-27 20:47:47 +0900

    Add Meal Domains

--- domain/src/main/java/com/dsm/dms/domain/usecase/BasicUseCase.kt
+++
@@ -1,1 +0,0 @@
-package com.dsm.dms.domain.usecase
---
+++ domain/src/main/java/com/dsm/dms/domain/usecase/GetMealUseCase.kt
@@ -0,0 +1,16 @@
+package com.dsm.dms.domain.usecase
+
+import com.dsm.dms.domain.base.UseCase
+import com.dsm.dms.domain.entity.Meal
+import io.reactivex.Single
+import com.dsm.dms.domain.base.Result
+import com.dsm.dms.domain.service.MealService
+import io.reactivex.disposables.CompositeDisposable
+
+class GetMealUseCase(
+    private val service: MealService,
+    composite: CompositeDisposable
+): UseCase<String, Result<Meal>>(composite) {
+    override fun create(date: String): Single<Result<Meal>> =
+        service.getMeal(date)
+}
