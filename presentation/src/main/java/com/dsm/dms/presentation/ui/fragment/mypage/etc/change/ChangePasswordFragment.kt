a6c68eb6b4ae69f37767e64b1596a931ac51a9ef
jinusong <sugwer7464@gmail.com>
2020-09-07 00:01:07 +0900

    Update ChangePassword Usecase connect

--- presentation/src/main/java/com/dsm/dms/presentation/ui/fragment/mypage/etc/change/ChangePasswordFragment.kt
+++ presentation/src/main/java/com/dsm/dms/presentation/ui/fragment/mypage/etc/change/ChangePasswordFragment.kt
@@ -9,6 +9,7 @@ import com.dsm.dms.presentation.base.DataBindingInjectFragment
 import com.dsm.dms.presentation.databinding.FragmentChangePasswordBinding
 import com.dsm.dms.presentation.viewmodel.main.mypage.etc.change.ChangePasswordViewModel
 import com.dsm.dms.presentation.viewmodel.main.mypage.etc.change.ChangePasswordViewModelFactory
+import kotlinx.android.synthetic.main.fragment_change_password.*
 import javax.inject.Inject
 
 
@@ -31,6 +32,15 @@ class ChangePasswordFragment: DataBindingInjectFragment<FragmentChangePasswordBi
     override fun observeEvent() {
         viewModel.backToMyPageEvent.observe(this, Observer {
             back()
+        })
+        viewModel.onLoadEvent.observe(this, Observer {
+            change_password_change_btn.onLoad(it)
+        })
+        viewModel.onSuccessEvent.observe(this, Observer {
+            change_password_change_btn.onSuccess(it)
+        })
+        viewModel.onErrorEvent.observe(this, Observer {
+            change_password_change_btn.onError(it)
         })
     }
 }
--- presentation/src/main/java/com/dsm/dms/presentation/viewmodel/main/mypage/etc/change/ChangePasswordViewModelFactory.kt
+++ presentation/src/main/java/com/dsm/dms/presentation/viewmodel/main/mypage/etc/change/ChangePasswordViewModelFactory.kt
@@ -2,10 +2,13 @@ package com.dsm.dms.presentation.viewmodel.main.mypage.etc.change
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.dsm.dms.domain.usecase.ChangePasswordUseCase
 
 
-class ChangePasswordViewModelFactory: ViewModelProvider.Factory {
+class ChangePasswordViewModelFactory(
+    private val changePasswordUseCase: ChangePasswordUseCase
+): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T
-            = modelClass.getConstructor().newInstance()
+            = modelClass.getConstructor(ChangePasswordUseCase::class.java).newInstance(changePasswordUseCase)
 
 }
